594cfa18f1c7bee6a9d7c18918ca63270e64fb05
Candy <candy@sh.gg>
2016-11-14 14:45:11 +0800

    del v

--- dist/index.js
+++ dist/index.js
@@ -5,20 +5,18 @@ var _commander = require('commander');
 
 var _commander2 = _interopRequireDefault(_commander);
 
-var _path = require('path');
-
-var _path2 = _interopRequireDefault(_path);
-
 var _query = require('./query');
 
 var _query2 = _interopRequireDefault(_query);
+
+var _package = require('../package.json');
+
+var _package2 = _interopRequireDefault(_package);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 console.log(__dirname);
-var file = _path2.default.join(__dirname, '../package.json');
-var pack = require(file);
-_commander2.default.version(pack.version).option('-m, --m [command]', '提交信息').parse(process.argv);
+_commander2.default.version(_package2.default.version).option('-m, --m [command]', '提交信息').parse(process.argv);
 
 _commander2.default.arguments('<type>').action(_query2.default).parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
 import program from 'commander';
 import queryFunc from './query';
-import pack from '../package.json';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-console.log(__dirname);
 program
- .version(pack.version)
  .option('-m, --m [command]', '提交信息')
  .parse(process.argv);
 
